ccc079cfc9ccf60bed2f89b336f99c62d544ed27
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-12 00:27:32 +0300

    генератор случайных чисел

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,7 +2,15 @@
 int numberB = 5;
 Console.WriteLine(numberB + numberB); */
 
+/* 
 int NumberA = 31;
 int numberB = 5;
+int result = (NumberA + numberB);
+Console.WriteLine(result); */
+
+int NumberA = new Random().Next(1, 40); // 1 2 3..9
+Console.WriteLine(NumberA);
+int numberB = new Random().Next(-20, 10);
+Console.WriteLine(numberB);
 int result = (NumberA + numberB);
 Console.WriteLine(result);
