a230cbdf97e606fc9edc57dc4863107dd925e2a1
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-11 23:36:56 +0300

    Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿/* int NumberA = 3;
+int numberB = 5;
+Console.WriteLine(numberB + numberB); */
+
+int NumberA = 31;
+int numberB = 5;
+int result = (NumberA + numberB);
+Console.WriteLine(result);
