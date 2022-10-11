880e8d3dc2dc2e0476937d48cb498d7a0938e1d9
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-12 01:11:03 +0300

    Example005_ifElse_Conditional

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine($"Максимум равен {max}");
