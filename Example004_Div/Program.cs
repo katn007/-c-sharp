0e2b78c6c51466f8a5b0a76fb7fa720f78a46ee8
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-12 00:00:42 +0300

    деление вечественных чисел

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12;
+double numberB = 5;
+//System.Console.Write(numberA / numberB);//
+Console.WriteLine($"Значение деления равно {numberA / numberB}");
