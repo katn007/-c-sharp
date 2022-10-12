1e8d0b47260c4bf8154a418472fdcc2ba9d9663f
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-12 20:43:26 +0300

    Первая задача

--- Домашнее задание 1/Program.cs
+++ Домашнее задание 1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+/*
+int a = 5;
+int b = 7;
+int max = a;
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число = {max}");
+}
+else
+{
+    max = b;
+}
+Console.WriteLine($"Максимальное число = {max}");
+
+int a = 2;
+int b = 10;
+int max = a;
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число = {max}");
+}
+else
+{
+    max = b;
+}
+Console.WriteLine($"Максимальное число = {max}");
+*/
+
+int a = -9;
+int b = -3;
+int max = a;
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число = {max}");
+}
+else
+{
+    max = b;
+}
+Console.WriteLine($"Максимальное число = {max}");
