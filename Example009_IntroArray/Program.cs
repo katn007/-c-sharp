4e96a1b7a8c78dcdbdebe2c171c634e3f34704ea
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-13 13:39:21 +0300

    array

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+//               0  1   2    3  4    5  6    7  8
+int[] array = { 101, 52, 93, 74, 55, 65, 57, 85, 9 };
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(max);
+
+
+
+
+
+
+
+/*
+int max = array[0];
+while (i < 9)
+{
+    if (max > array[i]) max = array[i];
+    i++;
+}
+
+
+Console.WriteLine(max);*/
