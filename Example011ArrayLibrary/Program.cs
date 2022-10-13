ddc1eb85f2adfcdf6c1cc8f2b6085685bd1a4ace
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-13 21:46:07 +0300

    fill array and print array

--- Example011ArrayLibrary/Program.cs
+++ Example011ArrayLibrary/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)// FillArray и collection - названия
+{
+    int lenght = collection.Length; //Длина массива "collection"
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(0, 50);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col) // методы (функции)- не возращает значения, не нужен ретерн
+{
+    int count = col.Length; // длина массива "col"
+    int position = 0; // position вместо index
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[20];// по умолчанию заполняется нулями
+
+FillArray(array);
+PrintArray(array);
