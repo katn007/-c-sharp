10781ae1c34a978c5e5b22387d31fb70d015c2ab
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-13 13:02:00 +0300

    Ввод в массивы

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿/* int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -15,10 +15,17 @@ int a3 = 13;
 int b3 = 2553;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 
 Console.WriteLine(max);
+*/
+
