afadcd1b7418caaca28c494011f06d9089b92ff1
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-22 18:03:27 +0300

    семинар 2

--- Домашнее задание 1/Program.cs
+++ Домашнее задание 1/Program.cs
@@ -30,7 +30,7 @@ else
     max = b;
 }
 Console.WriteLine($"Максимальное число = {max}");
-*/
+
 
 int a = -9;
 int b = -3;
@@ -43,4 +43,63 @@ else
 {
     max = b;
 }
-Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Максимальное число = {max}"); 
+*/
+
+/* Задача 
+ Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 
+
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.WriteLine($"Максимум равен {max}");
+
+
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет 
+*/
+double a;
+Console.WriteLine("Введите число ");
+double.TryParse(Console.ReadLine(), out a);
+if (a % 2 == 0)
+{
+    Console.WriteLine("это число четное");
+}
+else
+{
+    Console.WriteLine("это число НЕ четное");
+}
+
+
+
+
+
+/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 
+int a;
+Console.Write("Введите число ");
+int.TryParse(Console.ReadLine(), out a);
+int count = 2;
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count},");
+    }
+    count++;
+}
+Console.WriteLine(); 
+*/
