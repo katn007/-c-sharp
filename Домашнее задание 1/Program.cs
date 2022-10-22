30bcadbdf29f864031c33a1bbeb60ada0af7363b
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-23 00:12:26 +0300

    доделал

--- Домашнее задание 1/Program.cs
+++ Домашнее задание 1/Program.cs
@@ -4,6 +4,24 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
+//Решение на семинаре
+/*
+int a, b;
+Console.Write("Введите 1 число ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите 2 число ");
+int.TryParse(Console.ReadLine()!, out b);
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число = {a}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число = {b}");
+}*/
+
+
+
 /*
 int a = 5;
 int b = 7;
@@ -62,17 +80,34 @@ if (b > max) max = b;
 if (c > max) max = c;
 
 
-Console.WriteLine($"Максимум равен {max}");
+Console.WriteLine($"Максимум равен {max}");*/
+
+/*
+int a, b, c;
+a = new Random().Next(10);
+b = new Random().Next(10);
+c = new Random().Next(10);
+Console.Write($"{a} {b} {c} ");
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine($"Максимальное число = {max}");*/
 
 
-Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
-7 -> нет 
-*/
-double a;
+7 -> нет */
+
+
+/*int a;
 Console.WriteLine("Введите число ");
-double.TryParse(Console.ReadLine(), out a);
+int.TryParse(Console.ReadLine()!, out a);
 if (a % 2 == 0)
 {
     Console.WriteLine("это число четное");
@@ -81,25 +116,30 @@ else
 {
     Console.WriteLine("это число НЕ четное");
 }
-
+*/
 
 
 
 
 /* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 5 -> 2, 4
-8 -> 2, 4, 6, 8 
-int a;
+8 -> 2, 4, 6, 8 */
+/*int a;
 Console.Write("Введите число ");
 int.TryParse(Console.ReadLine(), out a);
 int count = 2;
 while (count <= a)
 {
-    if (count % 2 == 0)
-    {
-        Console.Write($"{count},");
-    }
-    count++;
+    Console.Write($"{count},");
+    count += 2;
 }
-Console.WriteLine(); 
-*/
+Console.WriteLine();*/
+
+int a;
+Console.Write("Введите число ");
+int.TryParse(Console.ReadLine(), out a);
+for (int i = 2; i <= a; i += 2)
+{
+    Console.Write($"{i}, ");
+}
+Console.WriteLine();
--- Семинар 2/рандом/Program.cs
+++ Семинар 2/рандом/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*for (int i = 0; i < 100; i++)
+{
+    int a = new Random().Next(5, 20);
+    Console.Write($"{a} ");
+
+}
+*/
+
+/* Напишите программу, которая выводит случайное число из отрезка [10,99]
+ и показывавет наибольшую цифру числа
+ 78 - 8 
+ 12 - 2
+ 85 - 8 
+
+int rand = new Random().Next(10, 100);
+
+Console.WriteLine(rand);
+int a = rand / 10;
+int b = rand % 10;
+Console.Write($"Наибольшая цифра числа = \n");
+if (a > b)
+    Console.Write(a);
+else
+    Console.Write(b);*/
+
+/* Напишите программу, которая выводит случайное трехзначное число 
+и удаляет вторую цифру этого числа
+int rand = new Random().Next(100, 1000);
+Console.WriteLine(rand);
+Console.WriteLine($"{rand / 100 * 10 + rand % 10}");*/
+
+//два числа а делим на б, делится или не делится - выводим остаток
+
+/*int _a = new Random().Next(10, 100);
+int _b = new Random().Next(10, 100);
+int ost = 0;
+Console.WriteLine($"{_a}");
+Console.WriteLine($"{_b}");*/
+
+/*int _a, _b;
+Console.WriteLine("Введите первое число");
+int.TryParse(Console.ReadLine()!, result: out _a);
+Console.WriteLine("Введите второе число");
+int.TryParse(Console.ReadLine()!, result: out _b);
+if (_a % _b == 0)
+    Console.WriteLine("Делится");
+else
+    Console.WriteLine($"Не Делится, остаток {_a % _b}");*/
+
+int a;
+Console.WriteLine("Введите  число");
+int.TryParse(Console.ReadLine()!, result: out a);
+if (a % 7 == 0 && a % 23 == 0)
+
+    Console.WriteLine("Делится\n");
+
+else
+    Console.WriteLine("yt Делится");
