11b79c8798240c982f02923922f505efea80908c
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-29 20:38:38 +0300

    циклы фоор

--- Example0012_Methods/Program.cs
+++ Example0012_Methods/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ВЛад");
+}
+//Method1();
+
+//Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+//Method21("Текст4434", 4);
+//Method21( );
+
+//Вид3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "qwerty");
+// Console.WriteLine(res);
+
+
+//Вид4.1
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "qwerty");
+// Console.WriteLine(res);
+*/
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} х {j} = {i * j}");
+    }
+
+}
