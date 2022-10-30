1d4fc5c185fd1bc788c60e8a53b3d27d4d02f871
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-30 12:52:31 +0200

    Рассторяние между координатами

--- Семинар 3/Program.cs
+++ Семинар 3/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int x = 0, y = 0;
+Console.Write($"Введите координату Х: ");
+int.TryParse(Console.ReadLine()!, out x);
+Console.Write($"Введите координату Y: ");
+int.TryParse(Console.ReadLine()!, out y);
+
+if (x > 0)
+{
+    if (y > 0)
+        Console.Write($"Плоскость 1");
+    else if (y < 0)
+        Console.Write($"Плоскость 4");
+    else
+        Console.Write($"Точка лежит на координате X");
+}
+else if (x < 0)
+{
+
+    if (y > 0)
+        Console.Write($"Плоскость 2");
+    else if (y < 0)
+        Console.Write($"Плоскость 3");
+    else
+        Console.Write($"Точка лежит на координате X");
+}
+else
+{
+    if (y == 0)
+        Console.Write($"Точка лежит В начале координат");
+    else
+        Console.Write($"Точка лежит на координате Y");
+}
+Console.WriteLine();
+*/
+
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.Write($"{i} ");
+// }
+
+int[] A = new int[2];
+int[] B = new int[2];
+
+// Console.Write($"Введите координату Х первой точки: ");
+// int.TryParse(Console.ReadLine()!, out A[0]);
+// Console.Write($"Введите координату Y первой точки: ");
+// int.TryParse(Console.ReadLine()!, out A[1]);
+// Console.Write($"Введите координату Х второй точки: ");
+// int.TryParse(Console.ReadLine()!, out B[0]);
+// Console.Write($"Введите координату Y второй точки: ");
+// int.TryParse(Console.ReadLine()!, out B[1]);
+
+// double c = Math.Sqrt((A[0] - B[0]) * (A[0] - B[0]) + (A[1] - B[1]) * (A[1] - B[1]));
+// Console.Write($"Расстояние между точками = {c}");
+
