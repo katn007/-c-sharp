6389c26eb80d2474b1e202423739b5ff85b8145d
katn007 <114080370+katn007@users.noreply.github.com>
2022-10-31 19:54:05 +0200

    урок 4

--- Example0012_Methods/Program.cs
+++ Example0012_Methods/Program.cs
@@ -63,7 +63,7 @@ Console.WriteLine(year);
 // }
 // string res = Method4(10, "qwerty");
 // Console.WriteLine(res);
-*/
+
 
 
 for (int i = 2; i <= 10; i++)
@@ -74,3 +74,68 @@ for (int i = 2; i <= 10; i++)
     }
 
 }
+*/
+//дан текст , заменить все пробелы черточками, мал к на большие К
+//а большие С на маленькие с
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом Согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+//string s = "qwerty"
+//            012
+// s[3] // "r"
+// String Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int lenght = text.Length;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
