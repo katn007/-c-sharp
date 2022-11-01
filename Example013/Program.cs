d6da2711ccf826edf6980dd25e1afd2a9f3523ce
katn007 <114080370+katn007@users.noreply.github.com>
2022-11-01 09:02:45 +0200

    матрица с генератором чисел

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -17,6 +17,8 @@ for (int rows = 0; rows < 2; rows++)
 }
 */
 
+
+
 void PrintArray(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
@@ -29,5 +31,18 @@ void PrintArray(int[,] matr)
     }
 }
 
-int[,] matrix = new int[3, 4];
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);//[1;10)
+        }
+    }
+}
+int[,] matrix = new int[3, 10];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
 PrintArray(matrix);
