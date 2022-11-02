3e5db7ac17b9e566af62a798700d43bfb965ff65
katn007 <114080370+katn007@users.noreply.github.com>
2022-11-02 19:44:03 +0200

    фибаначи

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -47,6 +47,7 @@ FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
 */
+/*
 
 int[,] pic = new int[,]
 {
@@ -102,3 +103,31 @@ void FillImage(int row, int col)
 PrintImage(pic);
 FillImage(13, 13);
 PrintImage(pic);
+
+
+// 1! = 1
+// 0! = 1
+double Factorial(double n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//Console.WriteLine(Factorial(5));//1*2*3=6 
+*/
+
+//f(1) = 1
+//f(2)=1
+//f(n)=f(n-1)+f(n-2)
+double Fibonacci(double n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (double i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
